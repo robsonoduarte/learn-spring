f84d5263fcc85f1bbf108f4bb437b3efc5065b14
Robson Duarte <robson.o.d@gmail.com>
2016-10-05 20:05:44 -0300

    add simple hello world of spring mvc

---
+++ book-store/src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+
+	public static void main(String[] args){
+		run(Application.class, args);
+	}
+
+}
