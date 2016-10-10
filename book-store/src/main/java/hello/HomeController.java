0804cc6cbbe5dde6ed07cfd45f8ba660666093b7
Robson Duarte <robson.o.d@gmail.com>
2016-10-10 20:15:00 -0300

    remove the defualt examples.

--- book-store/src/main/java/hello/HomeController.java
+++ book-store/src/main/java/hello/HomeController.java
@@ -1,19 +1,40 @@
 package hello;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-// the simple getting start from:  https://spring.io/guides/gs/serving-web-content/
+
 
 @Controller
-public class GreetingController{
+public class HomeController{
 
-	@RequestMapping("/greeting")
-	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model){
-		model.addAttribute("name", name);
-		return "greeting";
+	@RequestMapping("/ui-elements")
+	public String uiElements(){
+		return "ui-elements";
+	}
+
+
+	@RequestMapping("/chart")
+	public String charts(){
+		return "chart";
+	}
+
+
+	@RequestMapping("/tab-panel")
+	public String tabPanel(){
+		return "tab-panel";
+	}
+
+
+	@RequestMapping("/table")
+	public String table(){
+		return "tabel";
+	}
+
+
+	@RequestMapping("/form")
+	public String form(){
+		return "form";
 	}
 
 }
