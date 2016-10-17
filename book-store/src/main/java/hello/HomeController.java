df3752fad3fbca889730da1e257012e8eb72be3d
Robson Duarte <robson.o.d@gmail.com>
2016-10-17 19:52:13 -0200

    remove index.

--- book-store/src/main/java/hello/HomeController.java
+++ book-store/src/main/java/hello/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController{
 
+	
 	@RequestMapping("/ui-elements")
 	public String uiElements(){
 		return "ui-elements";
