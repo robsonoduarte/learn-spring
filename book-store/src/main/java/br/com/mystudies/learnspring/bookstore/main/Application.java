4ee892f713971b09ad75bff2f89999ce680bfa08
Robson Duarte <robson.o.d@gmail.com>
2018-06-26 21:44:16 -0300

    add @ComponentScan(basePackages="br.com.mystudies")

--- book-store/src/main/java/br/com/mystudies/learnspring/bookstore/main/Application.java
+++ book-store/src/main/java/br/com/mystudies/learnspring/bookstore/main/Application.java
@@ -3,8 +3,10 @@ package br.com.mystudies.learnspring.bookstore.main;
 import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="br.com.mystudies")
 public class Application{
 
 	public static void main(String[] args){
