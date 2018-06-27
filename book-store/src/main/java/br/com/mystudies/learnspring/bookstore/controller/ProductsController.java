6738a9bd132382df64428c86ee7ae16021969c56
Robson Duarte <robson.o.d@gmail.com>
2018-06-27 08:51:26 -0300

    format code.

--- book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/ProductsController.java
+++ book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/ProductsController.java
@@ -3,7 +3,6 @@ package br.com.mystudies.learnspring.bookstore.controller;
 import br.com.mystudies.learnspring.bookstore.domain.Product;
 
 public class ProductsController {
-
 	public String save(Product product) {
 		return "";
 	}
