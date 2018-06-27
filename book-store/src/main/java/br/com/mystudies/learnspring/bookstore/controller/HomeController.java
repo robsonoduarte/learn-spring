32b01217bcfc79c9bf3d9f4612554b22e275ecc6
Robson Duarte <robson.o.d@gmail.com>
2018-06-26 21:56:00 -0300

    return the page home

--- book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/HomeController.java
+++ book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/HomeController.java
@@ -1,18 +1,15 @@
 package br.com.mystudies.learnspring.bookstore.controller;
 
-import static org.springframework.http.HttpStatus.OK;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
 
 	@GetMapping()
-	@ResponseStatus(OK)
-	public void index() {
+	public String home() {
 		System.out.println("Carregando produtos...");
+		return "home";
 	}
 	
 }
