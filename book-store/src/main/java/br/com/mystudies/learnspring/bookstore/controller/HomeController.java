9ee4c851c7cbe1d3337403c505288d67da021cb2
Robson Duarte <robson.o.d@gmail.com>
2018-06-26 21:43:57 -0300

    add @ResponseStatus(OK)

--- book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/HomeController.java
+++ book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/HomeController.java
@@ -1,13 +1,18 @@
 package br.com.mystudies.learnspring.bookstore.controller;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
 
-	@GetMapping
+	@GetMapping()
+	@ResponseStatus(OK)
 	public void index() {
 		System.out.println("Carregando produtos...");
 	}
+	
 }
