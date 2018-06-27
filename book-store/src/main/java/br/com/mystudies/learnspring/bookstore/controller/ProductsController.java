0a567f1a632e98455bc3e340ac56f2a9caf6d578
Robson Duarte <robson.o.d@gmail.com>
2018-06-26 22:03:36 -0300

    new action in controller

--- book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/ProductsController.java
+++ book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/ProductsController.java
@@ -1,5 +1,11 @@
 package br.com.mystudies.learnspring.bookstore.controller;
 
+import br.com.mystudies.learnspring.bookstore.domain.Product;
+
 public class ProductsController {
 
+	
+	public String save(Product product) {
+		return "";
+	}
 }
