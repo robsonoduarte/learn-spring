cbd3476bb41f2dbe885feafa9045c1e820005cb7
Robson Duarte <robson.o.d@gmail.com>
2018-06-28 21:07:44 -0300

    add the new route.

--- book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/ProductsController.java
+++ book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/ProductsController.java
@@ -2,6 +2,7 @@ package br.com.mystudies.learnspring.bookstore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.mystudies.learnspring.bookstore.domain.Product;
 
@@ -11,10 +12,11 @@ public class ProductsController {
 
 	@GetMapping(value="/products/new")
 	public String form() {
-		return "/products/form";
+		return "/products/new";
 	}
 
 
+	@PostMapping(value="/products/save")
 	public String save(Product product) {
 		return "";
 	}
