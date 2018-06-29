aa4e48537cb24e1396272003a75bcb359cb496db
Robson Duarte <robson.o.d@gmail.com>
2018-06-29 20:05:53 -0300

    add route

--- book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/HomeController.java
+++ book-store/src/main/java/br/com/mystudies/learnspring/bookstore/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping()
+	@GetMapping("/home")
 	public String home() {
 		System.out.println("Carregando produtos...");
 		return "home";
-	}	
+	}
 }
