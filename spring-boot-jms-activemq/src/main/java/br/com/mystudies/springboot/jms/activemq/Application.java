ba37a2dc7841b76988d0c0ee30efb2c4eb0fe38a
Robson Duarte <robson.o.d@gmail.com>
2018-07-20 10:14:33 -0300

    format code.

--- spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/Application.java
+++ spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/Application.java
@@ -1,13 +1,12 @@
 package br.com.mystudies.springboot.jms.activemq;
 
-import org.springframework.boot.SpringApplication;
+import static org.springframework.boot.SpringApplication.run;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OntrackerApiNewtecApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(OntrackerApiNewtecApplication.class, args);
-	}
-	
+		run(Application.class, args);
+	}	
 }
