0c37521e7b0d800344bce3386828b9ed63b653b5
Robson Duarte <robson.o.d@gmail.com>
2018-07-20 10:07:26 -0300

    add new task

--- book-store/src/main/java/hello/TODO.java
+++ book-store/src/main/java/hello/TODO.java
@@ -1,7 +1,6 @@
 package hello;
 
 public class TODO {
-
 	// TODO:use Thymeleaf fragments in template to header / menu / footer... ( http://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html#fragments )
 	// TODO:criate common template
 }
