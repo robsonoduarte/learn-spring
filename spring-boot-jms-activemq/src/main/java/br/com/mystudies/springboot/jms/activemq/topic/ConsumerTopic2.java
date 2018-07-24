6c8d97ddee62e8f2ebd8a3d946f2fd0d4330ccb3
Robson Duarte <robson.o.d@gmail.com>
2018-07-24 19:00:10 -0300

    rename class

---
+++ spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/topic/ConsumerTopic2.java
@@ -0,0 +1,18 @@
+package br.com.mystudies.springboot.jms.activemq.topic;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import br.com.mystudies.springboot.jms.activemq.domain.Temp;
+
+@Component
+public class ConsumerTopic2 {
+
+	
+	
+	
+	@JmsListener(destination="temp.topic", containerFactory="topicListenerFactory")
+	public void receive(Temp temp) {
+		System.out.println("Recebendo no consumer 2 => " + temp);
+	}
+}
