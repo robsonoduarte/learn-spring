53f40b7b1180dffee1a48fb996bf817b7ec3335f
Robson Duarte <robson.o.d@gmail.com>
2018-07-24 19:00:29 -0300

    rename class

---
+++ spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/queue/ConsumerQueue2.java
@@ -0,0 +1,18 @@
+package br.com.mystudies.springboot.jms.activemq.queue;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import br.com.mystudies.springboot.jms.activemq.domain.Temp;
+
+@Component
+public class ConsumerQueue2 {
+
+	
+	@JmsListener(destination="temp.queue", containerFactory="queueListenerFactory")
+	public void consume(Temp temp){
+		System.out.println("Queue Consumer2 ->" + temp);
+	}
+	
+	
+}
