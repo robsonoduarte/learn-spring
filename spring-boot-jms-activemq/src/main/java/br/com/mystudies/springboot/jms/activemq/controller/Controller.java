5c08e4956f76d4418e24ad33bb20534e11a15b36
Robson Duarte <robson.o.d@gmail.com>
2018-07-24 18:59:48 -0300

    rename class

---
+++ spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/controller/Controller.java
@@ -0,0 +1,34 @@
+package br.com.mystudies.springboot.jms.activemq.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mystudies.springboot.jms.activemq.domain.Temp;
+import br.com.mystudies.springboot.jms.activemq.queue.SenderQueue;
+import br.com.mystudies.springboot.jms.activemq.topic.SenderTopic;
+
+@RestController
+public class Controller {
+
+	@Autowired
+	private SenderTopic ts;
+	
+	@Autowired
+	private SenderQueue qs;
+	
+	@GetMapping("topic")
+	public void topic() {
+		Temp temp = new Temp();
+		temp.setName("temp");
+		ts.send(temp);
+	}
+	
+	
+	@GetMapping("queue")
+	public void queue() {
+		Temp temp = new Temp();
+		temp.setName("temp");
+		qs.send(temp);
+	}
+}
