170bd78cc253eb364d0ec122fe91584893e558c8
robsonoduarte <robson.o.d@gmail.com>
2018-07-26 20:37:10 -0300

    raname package

--- spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/controller/BooksController.java
+++ spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/controller/BooksController.java
@@ -1,4 +1,4 @@
-package br.com.mystudies.learnspring.bookstore.controller;
+package br.com.mystudies.springboot.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
