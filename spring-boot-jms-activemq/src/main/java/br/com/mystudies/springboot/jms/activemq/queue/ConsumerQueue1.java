5ce303340387070b382f47341e498648a1d22e1d
robsonoduarte <robson.o.d@gmail.com>
2018-08-02 21:16:35 -0300

    use new message

--- spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/queue/ConsumerQueue1.java
+++ spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/queue/ConsumerQueue1.java
@@ -3,14 +3,14 @@ package br.com.mystudies.springboot.jms.activemq.queue;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import br.com.mystudies.springboot.jms.activemq.domain.Temp;
+import br.com.mystudies.springboot.jms.activemq.domain.Message;
 
 @Component
 public class ConsumerQueue1 {
 
 	
 	@JmsListener(destination="temp.queue", containerFactory="queueListenerFactory")
-	public void consume(Temp temp){
+	public void consume(Message temp){
 		System.out.println("Queue Consumer1 ->" + temp);
 	}
 	
