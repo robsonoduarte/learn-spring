228e7d250e90cf1b676de0c8ef1734f2cfc28faa
robsonoduarte <robson.o.d@gmail.com>
2018-08-02 21:16:45 -0300

    use new message

--- spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/topic/ConsumerTopic1.java
+++ spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/topic/ConsumerTopic1.java
@@ -3,7 +3,7 @@ package br.com.mystudies.springboot.jms.activemq.topic;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import br.com.mystudies.springboot.jms.activemq.domain.Temp;
+import br.com.mystudies.springboot.jms.activemq.domain.Message;
 
 @Component
 public class ConsumerTopic1 {
@@ -12,7 +12,7 @@ public class ConsumerTopic1 {
 	
 	
 	@JmsListener(destination="temp.topic", containerFactory="topicListenerFactory")
-	public void receive(Temp temp) {
+	public void receive(Message temp) {
 		System.out.println("Recebendo no consumer 1 => " + temp);
 	}
 }
