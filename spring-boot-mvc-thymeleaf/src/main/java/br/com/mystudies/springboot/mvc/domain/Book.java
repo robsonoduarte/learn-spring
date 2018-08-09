65ac6dfcb3b022f8e4321255ffe9e9cf3340dbef
robsonoduarte <robson.o.d@gmail.com>
2018-08-09 20:23:52 -0300

    add jpa mapeded in book class.

--- spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/domain/Book.java
+++ spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/domain/Book.java
@@ -1,8 +1,28 @@
 package br.com.mystudies.springboot.mvc.domain;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="BOOK")
 public class Book {
+
+
+	@Column(name="ID")
+	@GeneratedValue(strategy=IDENTITY)
+	private Long id;
+
+	@Column(name="TITLE")
 	public String title;
+
+	@Column(name="DESCRIPTION")
 	public String description;
+
+	@Column(name="PAGES")
 	public int pages;
-	
+
 }
