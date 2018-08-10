0c1c628f900e9ec9a7ea37ea7d04c0e39e05b06a
robsonoduarte <robson.o.d@gmail.com>
2018-08-10 09:50:57 -0300

    format code.

--- spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/topic/ConsumerTopic2.java
+++ spring-boot-jms-activemq/src/main/java/br/com/mystudies/springboot/jms/activemq/topic/ConsumerTopic2.java
@@ -8,9 +8,6 @@ import br.com.mystudies.springboot.jms.activemq.domain.Message;
 @Component
 public class ConsumerTopic2 {
 
-	
-	
-	
 	@JmsListener(destination="topic", containerFactory="topicListenerFactory")
 	public void receive(Message message) {
 		System.out.println("Consumer Topic 2 => " + message);
