d875b723e7a11394644f939bad266786d555d967
robsonoduarte <robson.o.d@gmail.com>
2018-08-09 21:31:45 -0300

    book service

---
+++ spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/service/BookService.java
@@ -0,0 +1,23 @@
+package br.com.mystudies.springboot.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mystudies.springboot.mvc.domain.Book;
+import br.com.mystudies.springboot.mvc.repository.BookRepository;
+
+@Service
+public class BookService {
+
+	@Autowired
+	private BookRepository repository;
+
+
+
+
+	public Book save(Book book) {
+		return repository.save(book);
+	}
+
+
+}
