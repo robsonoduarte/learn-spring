51ab37e0b7248c993f8c7182a8bf385ad4a44959
robsonoduarte <robson.o.d@gmail.com>
2018-08-09 21:31:54 -0300

    book repository

---
+++ spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package br.com.mystudies.springboot.mvc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.mystudies.springboot.mvc.domain.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
