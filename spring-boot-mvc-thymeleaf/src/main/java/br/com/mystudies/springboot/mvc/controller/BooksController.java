717e5a74c5164718d8b6d000d24d08caad24984a
robsonoduarte <robson.o.d@gmail.com>
2018-08-09 21:32:55 -0300

    add service.

--- spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/controller/BooksController.java
+++ spring-boot-mvc-thymeleaf/src/main/java/br/com/mystudies/springboot/mvc/controller/BooksController.java
@@ -1,22 +1,36 @@
 package br.com.mystudies.springboot.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.mystudies.springboot.mvc.domain.Book;
+import br.com.mystudies.springboot.mvc.service.BookService;
 
 @Controller
 @RequestMapping("/books")
 public class BooksController {
+
+	@Autowired
+	private BookService service;
+
+
 
 	@GetMapping(value="/form")
 	public String form() {
 		return "/books/form";
 	}
 
-	@PostMapping(value="/save")
+	/*@PostMapping(value="/save")*/
+	@GetMapping(value="/save")
 	public String save() {
-		return "";
+		Book book = new Book();
+		book.title = "test";
+		book.description = "test";
+		book.pages = 1 ;
+		service.save(book);
+		return "/books/list";
 	}
 
 	@GetMapping(value="/list")
