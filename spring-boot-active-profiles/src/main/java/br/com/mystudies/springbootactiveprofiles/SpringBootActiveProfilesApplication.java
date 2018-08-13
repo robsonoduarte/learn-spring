4f1a307c1291b2448c1fdc842abb3a7040c82eae
robsonoduarte <robson.o.d@gmail.com>
2018-08-13 10:33:42 -0300

    commit initial of project.

---
+++ spring-boot-active-profiles/src/main/java/br/com/mystudies/springbootactiveprofiles/SpringBootActiveProfilesApplication.java
@@ -0,0 +1,12 @@
+package br.com.mystudies.springbootactiveprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActiveProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActiveProfilesApplication.class, args);
+	}
+}
