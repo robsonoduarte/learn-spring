1cc3980a2110ea6dba28ee9d50eb7920a3bc7ac9
robsonoduarte <robson.o.d@gmail.com>
2018-10-10 14:47:10 -0300

    loud source in class path

--- spring-boot-data-mongo/src/main/java/br/com/mystudies/springboot/dataloader/SourceLoader.java
+++ spring-boot-data-mongo/src/main/java/br/com/mystudies/springboot/dataloader/SourceLoader.java
@@ -5,7 +5,6 @@ import static java.util.stream.Collectors.toList;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -17,19 +16,16 @@ public class SourceLoader {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SourceLoader.class);
 
-	private static final String SOURCE_URL = "http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-2";
-
-
 	public List<String> loadLines() throws Exception {
 
-		LOGGER.info("Starting loading the source by HTTP");
+		LOGGER.info("Starting loading the source...");
 
 		List<String> lines =
-				new BufferedReader(new InputStreamReader(new URL(SOURCE_URL).openStream(),UTF_8))
+				new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/source/source"),UTF_8))
 				.lines()
 				.collect(toList());
 
-		LOGGER.info("Finishing loading of source by HTTP");
+		LOGGER.info("Finishing loading the source");
 
 		return lines;
 	}
