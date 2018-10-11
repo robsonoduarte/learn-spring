654fdb820c06636a4e4d9424cc37f97b097c828f
robsonoduarte <robson.o.d@gmail.com>
2018-10-11 11:11:46 -0300

    initial import off projet

---
+++ spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/GreetingHandler.java
@@ -0,0 +1,19 @@
+package br.com.mystudies.springboot;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingHandler {
+
+	public Mono<ServerResponse> hello(ServerRequest request){
+		return ok().contentType(TEXT_PLAIN).body(fromObject("Hello, Spring"));
+	}
+}
---
+++ spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/SpringBootReactiveRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.mystudies.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveRestApplication.class, args);
+	}
+}
