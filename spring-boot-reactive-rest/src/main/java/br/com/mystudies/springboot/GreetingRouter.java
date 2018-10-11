70690837909c605df63a90a0d2ba80558443a962
robsonoduarte <robson.o.d@gmail.com>
2018-10-11 11:21:20 -0300

    add Router

---
+++ spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/GreetingRouter.java
@@ -0,0 +1,19 @@
+package br.com.mystudies.springboot;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class GreetingRouter {
+	@Bean
+	public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler){
+		return RouterFunctions.route(GET("/hello").and(accept(TEXT_PLAIN)),greetingHandler::hello);
+	}
+}
