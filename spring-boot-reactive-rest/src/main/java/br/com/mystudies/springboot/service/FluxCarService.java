5ef8629bcc99726c5340e557f436a92600b17fac
YASUDA\gtc.roduarte <gtc.roduarte@sompo.com.br>
2018-10-16 14:05:33 -0300

    new service.

---
+++ spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/service/FluxCarService.java
@@ -0,0 +1,42 @@
+package br.com.mystudies.springboot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mystudies.springboot.domain.Car;
+import br.com.mystudies.springboot.repo.CarRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class FluxCarService {
+
+
+	@Autowired
+	private CarRepository repository;
+
+
+	public Flux<Car> all(){
+		return repository.findAll();
+	}
+
+
+
+	public Mono<Car> byId(String id){
+		return repository.findById(id);
+	}
+
+
+
+
+
+/*	public Flux<Car> streams(String id){
+
+	}*/
+
+
+
+
+
+
+}
