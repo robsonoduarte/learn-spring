f9c549dc818d6350434fcee03beb2accdc0d2648
robsonoduarte <robson.o.d@gmail.com>
2018-10-19 14:26:06 -0300

    test read lines

--- spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/Temp.java
+++ spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/Temp.java
@@ -1,7 +1,16 @@
 package br.com.mystudies.springboot;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Temp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+
+		Files.lines(Paths.get(Temp.class.getResource("/source/cars").toURI())).forEach( l ->{
+						System.out.println(l);
+					}
+				);
+
 	}
 }
