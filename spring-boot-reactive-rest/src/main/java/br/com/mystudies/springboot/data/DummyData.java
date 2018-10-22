ec7b9d7c4f5270807dea90fb4bfc79eabf005963
robsonoduarte <robson.o.d@gmail.com>
2018-10-22 16:52:57 -0200

    comment code.

--- spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/data/DummyData.java
+++ spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/data/DummyData.java
@@ -8,6 +8,11 @@ public class DummyData implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+/*		Files.lines(Paths.get(Temp.class.getResource("/source/cars").toURI()))
+			.map(line -> asList(line.split(",")).stream())
+			.flatMap(car -> new Car("aaa", car));*/
+
 		// TODO Auto-generated method stub
 		System.out.println(" 1 2 3 ....");
 	}
