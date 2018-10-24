9e5182c6c35a8282e8673b89a8c6fa6e99834299
robsonoduarte <robson.o.d@gmail.com>
2018-10-24 12:50:46 -0200

    remove sysout.

--- spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/Application.java
+++ spring-boot-reactive-rest/src/main/java/br/com/mystudies/springboot/Application.java
@@ -4,13 +4,9 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.mystudies.springboot.client.GreetingWebClient;
-
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		run(Application.class, args);
-		System.out.println(new GreetingWebClient().getResult());
 	}
 }
