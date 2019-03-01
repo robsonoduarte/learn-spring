abd23c996bece98cba1e380548d4ba6b5af9c266
robsonoduarte <robson.o.d@gmail.com>
2019-02-28 23:08:23 -0300

    commit initial of project

---
+++ spring-boot-data-r2dbc/src/main/kotlin/br/com/mystudies/spring/springbootdatar2dbc/SpringBootDataR2dbcApplication.kt
@@ -0,0 +1,11 @@
+package br.com.mystudies.spring.springbootdatar2dbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootDataR2dbcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootDataR2dbcApplication>(*args)
+}
---
+++ spring-boot-data-r2dbc/src/test/kotlin/br/com/mystudies/spring/springbootdatar2dbc/SpringBootDataR2dbcApplicationTests.kt
@@ -0,0 +1,16 @@
+package br.com.mystudies.spring.springbootdatar2dbc
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootDataR2dbcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
