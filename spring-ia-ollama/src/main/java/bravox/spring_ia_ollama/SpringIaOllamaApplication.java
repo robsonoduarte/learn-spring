592d02d5129a7eeec4bcb728208bff3d99080abd
robsonoduarte <robson.o.d@gmail.com>
2025-06-21 01:43:18 -0300

    spring ia with ollama

---
+++ spring-ia-ollama/src/main/java/bravox/spring_ia_ollama/SpringIaOllamaApplication.java
@@ -0,0 +1,13 @@
+package bravox.spring_ia_ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIaOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIaOllamaApplication.class, args);
+	}
+
+}
---
+++ spring-ia-ollama/src/test/java/bravox/spring_ia_ollama/SpringIaOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package bravox.spring_ia_ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIaOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
