6c2226ecc9305b2cc8aa3d6a448eca37c54981e9
robsonoduarte <robson.o.d@gmail.com>
2025-08-23 01:16:57 -0300

    spring ia course - initial project

--- spring-ia-course/src/main/java/br/com/bravox/Application.java
+++ spring-ia-course/src/main/java/br/com/bravox/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringIaCourseApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIaCourseApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- spring-ia-course/src/test/java/br/com/bravox/ApplicationTests.java
+++ spring-ia-course/src/test/java/br/com/bravox/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringIaCourseApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
