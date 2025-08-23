65e8464d50082bee4d3d262498d7b6cbf80e7227
Robson Duarte <robson.o.d@gmail.com>
2025-08-23 01:18:49 -0300

    spring ia course - the  first chat controller (#2)

---
+++ spring-ia-course/src/main/java/br/com/bravox/ChatController.java
@@ -0,0 +1,41 @@
+package br.com.bravox;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat() {
+        return chatClient.prompt()
+                .user("Tell me an interesting fact about java")
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream() {
+        return chatClient.prompt()
+                .user("I'm visiting Hilton Head soon, can you give me 10 places I must visit")
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/joke")
+    public ChatResponse joke(){
+        return chatClient.prompt()
+                .user("Tell me a joke about cats")
+                .call()
+                .chatResponse();
+    }
+}
