83c4ad7e8aa0c1e60afd997c9aa35edc77ed7bba
robsonoduarte <robson.o.d@gmail.com>
2025-09-13 01:24:56 -0300

    spring ia course - structured output

---
+++ spring-ia-course/src/main/java/br/com/bravox/output/Itinerary.java
@@ -0,0 +1,6 @@
+package br.com.bravox.output;
+
+import java.util.List;
+
+public record Itinerary(List<Activity> activities) {
+}
---
+++ spring-ia-course/src/main/java/br/com/bravox/output/VacationPlan.java
@@ -0,0 +1,34 @@
+package br.com.bravox.output;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VacationPlan {
+
+    private final ChatClient chatClient;
+
+    public VacationPlan(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/vacation/unstructured")
+    public String unstructured(){
+        return chatClient.prompt()
+                .user("What's a good vacation plan while I'm in Montreal CA for 4 days?")
+                .call()
+                .content();
+    }
+    @GetMapping("/vacation/structured")
+    public Itinerary structured(@RequestParam(value = "destination", defaultValue =  "Cleveland, OH") String destination){
+        return chatClient.prompt()
+                .user(u -> {
+                    u.text("What's a good vacation plan while I'm in {destination} for 4 days?");
+                    u.param("destination", destination);
+                })
+                .call()
+                .entity(Itinerary.class);
+    }
+}
