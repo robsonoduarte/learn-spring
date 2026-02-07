f332c03ca6dbd7565c5e7ddd063bde699848040b
Robson Duarte <robson.o.d@gmail.com>
2026-02-07 02:14:11 -0300

    spring ia course - rag (#14)

---
+++ spring-ia-course/src/main/java/br/com/bravox/rag/Model.java
@@ -0,0 +1,4 @@
+package br.com.bravox.rag;
+
+public record Model(String company, String model, int contextWindowSize) {
+}
---
+++ spring-ia-course/src/main/java/br/com/bravox/rag/Models.java
@@ -0,0 +1,6 @@
+package br.com.bravox.rag;
+
+import java.util.List;
+
+public record Models(List<Model> models) {
+}
