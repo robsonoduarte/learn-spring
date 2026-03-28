bc7d0f666cf42a4368ff4456b99710cb697dbd5d
robsonoduarte <robson.o.d@gmail.com>
2026-03-28 01:51:21 -0300

    spring ai - mcp server

---
+++ spring-ai-mcp-stdio/src/test/java/br/bravox/spring_ia_mcp_stdio/SpringIaMcpStdioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.bravox.spring_ia_mcp_stdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIaMcpStdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
