e8dc0504b0f62788d4b077363934f4856960d368
robsonoduarte <robson.o.d@gmail.com>
2026-05-15 01:53:14 -0300

    spring ai - metrics

---
+++ spring-ai-metrics /src/main/java/bravox/dev/SpringAiMetricsApplication.java
@@ -0,0 +1,13 @@
+package bravox.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiMetricsApplication.class, args);
+	}
+
+}
---
+++ spring-ai-metrics /src/test/java/bravox/dev/SpringAiMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package bravox.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
